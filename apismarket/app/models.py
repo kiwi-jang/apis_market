0df0efc68af189e43ded4653cdbdfa71a7daa6e3
kiwi-jang <76686974+kiwi-jang@users.noreply.github.com>
2021-03-06 23:22:00 +0900

    Update models.py

--- apismarket/app/models.py
+++ apismarket/app/models.py
@@ -1,30 +1,14 @@
 from app import db
 
-class Selling(db.Model):
+class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    #판매 고유번호
-    
     subject = db.Column(db.String(200), nullable=False)
-    #판매 제목
-    
     content = db.Column(db.Text(), nullable=False)
-    #판매 내용
-    
     create_date = db.Column(db.DateTime(), nullable=False)
-    #작성 일시
     
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    #답변 데이터의 고유 번호
-    
-    Selling_id = db.Column(db.Integer, db.ForeignKey('Selling.id', ondelete='CASCADE'))
-    #판매 데이터의 고유 번호 
-    
-    Selling = db.relationship('Selling', backref=db.backref('answer_set'))
-    #참조할 질문 모델
-    
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
+    question = db.relationship('Question', backref=db.backref('answer_set'))
     content = db.Column(db.Text(), nullable=False)
-    #대답 내용
-    
     create_date = db.Column(db.DateTime(), nullable=False)
-    #작성 일시
