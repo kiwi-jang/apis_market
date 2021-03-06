d48d8388e4456886e2339703b35ef52c56cb913a
kiwi-jang <76686974+kiwi-jang@users.noreply.github.com>
2021-03-06 23:42:39 +0900

    Update forms.py

--- apismarket/app/forms.py
+++ apismarket/app/forms.py
@@ -7,3 +7,9 @@ class UserLoginForm(FlaskFrom):
   username = StringField('사용자 이름', validators=[DataRequired(), Length(min=3, max=25)])
   password = PasswordField('비밀번호',validators=[DateRequired()])
   
+class UserCreateForm(FlaskForm):
+    username = StringField('사용자이름', validators=[DataRequired(), Length(min=3, max=25)])
+    password1 = PasswordField('비밀번호', validators=[
+        DataRequired(), EqualTo('password2', '비밀번호가 일치하지 않습니다')])
+    password2 = PasswordField('비밀번호확인', validators=[DataRequired()])
+    email = EmailField('이메일', [DataRequired(), Email()])
