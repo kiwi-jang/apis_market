d1db962d8cf8da7fb2397f2804852d21b8b22cd6
kiwi-jang <76686974+kiwi-jang@users.noreply.github.com>
2021-03-06 19:24:54 +0900

    Update __init__.py

--- apismarket/app/__init__.py
+++ apismarket/app/__init__.py
@@ -11,13 +11,13 @@ def create_app():
     app = Flask(__name__)
     
      app.config.from_object(config)
-
     # ORM
+    
     db.init_app(app)
     migrate.init_app(app, db)
-
     # 블루프린트
     
+    from . import models
     from .views import main_views
     app.register_blueprint(main_views.bp)
     
